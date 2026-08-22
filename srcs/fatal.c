1d46629eb2335c8e3fd2ad671b260e360f8571b8
Fonh Bui Min <fbui-min@3-E-5.42heilbronn.de>
2026-08-22 18:04:20 +0200

    printing a proper nextline

--- srcs/fatal.c
+++ srcs/fatal.c
@@ -6,7 +6,7 @@
 /*   By: fbui-min <fbui-min@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/22 15:37:22 by fbui-min          #+#    #+#             */
-/*   Updated: 2026/08/22 15:41:03 by fbui-min         ###   ########.fr       */
+/*   Updated: 2026/08/22 18:03:20 by fbui-min         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,19 @@ void	fatal(char *msg, t_program *prog)
 	exit(EXIT_FAILURE);
 }
 
+
 void	fatal_line(char *line, int line_no, t_program *prog)
 {
+	size_t	len;
+
 	if (prog)
 		cleanup_program(prog);
 	ft_putstr_fd("Error\nParse error at line ", 2);
 	ft_putnbr_fd(line_no, 2);
 	ft_putstr_fd(":\n    ", 2);
 	ft_putstr_fd(line, 2);
-	ft_putstr_fd("\n", 2);
+	len = ft_strlen(line);
+	if (len == 0 || line[len - 1] != '\n')
+		ft_putstr_fd("\n", 2);
 	exit(EXIT_FAILURE);
 }
